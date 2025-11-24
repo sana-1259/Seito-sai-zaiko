cf9134b839a6bf476bc391892578afdea35b20e8
sana-1259 <sana2744@icloud.com>
2025-11-24 13:27:42 +0900

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 
-// HTMLファイルのID="root"の要素に、Appコンポーネントを挿入して描画します。
-// これが、App.jsxのコードをウェブサイトとして表示するための「結び目」です。
+// ブラウザのDOMにReactコンポーネントをマウント（取り付け）する標準的なファイル
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
